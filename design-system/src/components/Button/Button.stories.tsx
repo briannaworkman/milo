a810e4d375787e5f74f803bd7738f21a7d83055c
Bri Workman <107228348+briannaworkman@users.noreply.github.com>
2025-04-15 21:34:16 -0400

    MILO-2: Buttons (#7)
    * Creates button component & primary variant
    
    * Adds primary button story
    
    * Adds secondary button styles & story
    
    * Adds outline button styles & story
    
    * Adds link button styles & story

---
+++ design-system/src/components/Button/Button.stories.tsx
@@ -0,0 +1,39 @@
+import Button from './Button';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Components/Button',
+};
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Primary Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary Button',
+    variant: 'secondary',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: 'Outline Button',
+    variant: 'outline',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    children: 'Link Button',
+    variant: 'link',
+    size: 'link',
+  },
+};
+
+export default meta;
