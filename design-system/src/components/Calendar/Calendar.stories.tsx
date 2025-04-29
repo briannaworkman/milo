d4edcf884e68d463feb317ee8e792213c539faaf
Bri Workman <107228348+briannaworkman@users.noreply.github.com>
2025-04-28 20:02:20 -0400

    Adds calendar story

--- design-system/src/components/Calendar/Calendar.stories.tsx
+++ design-system/src/components/Calendar/Calendar.stories.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { Calendar } from './Calendar';
 import { Meta, StoryObj } from '@storybook/react';
 
+const CalendarComponent = () => {
+  const [selected, setSelected] = useState<Date>(new Date('2025-04-02'));
+
+  return (
+    <Calendar
+      mode="single"
+      required={true}
+      selected={selected}
+      onSelect={setSelected}
+    />
+  );
+};
+
 const meta: Meta<typeof Calendar> = {
-  component: Calendar,
+  component: CalendarComponent,
   title: 'Components/Calendar',
 };
 
 type Story = StoryObj<typeof Calendar>;
 
-export const Primary: Story = {
-  args: {},
-};
+export const Primary: Story = {};
 
 export default meta;
